9de4038385ecb1500af982e3d1ae552bf46ca391
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2026-05-06 20:34:56 -0300

    Initial Commit

---
+++ MiniFood/MiniFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniFoodApp.swift
+//  MiniFood
+//
+//  Created by Thiago Lourenço on 06/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MiniFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
