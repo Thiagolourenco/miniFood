df8715a2c11f44f20e2bfecd0f1730164907d986
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2026-05-11 07:18:29 -0300

    feat: Added menu bottom in footer

---
+++ MiniFood/Core/Model/TabItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  TabItemModel.swift
+//  MiniFood
+//
+//  Created by Thiago Lourenço on 10/05/26.
+//
+
+import Foundation
---
+++ MiniFood/Core/View/TabView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  MiniFood
+//
+//  Created by Thiago Lourenço on 10/05/26.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
