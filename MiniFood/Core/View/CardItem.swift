a53180cc99ca8ef1a3fabf0fdf981e8d1fcb174d
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2026-05-13 13:25:06 -0300

    feat: Creating view to Card product

---
+++ MiniFood/Core/View/CardItem.swift
@@ -0,0 +1,18 @@
+//
+//  CardItem.swift
+//  MiniFood
+//
+//  Created by Thiago Lourenço on 13/05/26.
+//
+
+import SwiftUI
+
+struct CardItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardItem()
+}
