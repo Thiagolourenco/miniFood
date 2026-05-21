d3d021d9bf25d407d38adeded5954e1db01f4f18
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2026-05-21 10:43:19 -0300

    feat: integrate with endpoint to register user and navigate to home when user can register

---
+++ MiniFood/Core/ContinueWithEmail/Model/ApiResponse.swift
@@ -0,0 +1,8 @@
+//
+//  ApiResponse.swift
+//  MiniFood
+//
+//  Created by Thiago Lourenço on 21/05/26.
+//
+
+import Foundation
---
+++ MiniFood/Networking/Error/AuthServiceError.swift
@@ -0,0 +1,8 @@
+//
+//  AuthServiceError.swift
+//  MiniFood
+//
+//  Created by Thiago Lourenço on 21/05/26.
+//
+
+import Foundation
