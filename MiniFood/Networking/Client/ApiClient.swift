04d3f8ce5903d3802031fc12490e9a6da1818440
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2026-06-05 14:51:33 -0300

    feat: update

--- MiniFood/Networking/Client/ApiClient.swift
+++ MiniFood/Networking/Client/ApiClient.swift
@@ -12,7 +12,6 @@ struct ApiClient {
     var session: URLSession = .shared
     var decoder: JSONDecoder = JSONDecoder()
             
-    // # TODO: Improve this func, separate 
     func execute<Response>(_ requestModel: ApiRequest<Response>) async throws -> Response {
         var headers: [String: String] = [:]
 
@@ -29,30 +28,13 @@ struct ApiClient {
         }
         
         if httpResponse.statusCode == 401 {
-            try await refreshToken()
+            let resulRetry = try await AuthInterceptors<Response>(
+                baseURL: baseURL,
+                decoder: decoder,
+                requestModel: requestModel,
+                headers: headers).retry()
             
-            if let accessToken = KeychainService.shared.read(key: "accessToken"), !accessToken.isEmpty {
-                headers["Authorization"] = "Bearer \(accessToken)"
-            }
-            
-            let request = try requestModel.makeUrlRequest(defaultHeaders: headers)
-            
-            let (data, httpResponse) = try await URLSession.shared.data(for: request)
-            
-            guard let retryReponse = httpResponse as? HTTPURLResponse else {
-                throw URLError(.badServerResponse)
-            }
-            
-            guard (200...299).contains(retryReponse.statusCode) else {
-                let apiError = try? decoder.decode(ApiResponseError.self, from: data)
-                
-                throw AuthServiceError.api(
-                    message: apiError?.error.message ?? "Request failed with status code \(retryReponse.statusCode)"
-                )
-                
-            }
-            
-            return try JSONDecoder().decode(Response.self, from: data)
+            return resulRetry
         }
         
         if !(200...299).contains(httpResponse.statusCode) {
@@ -63,60 +45,5 @@ struct ApiClient {
 
         return try JSONDecoder().decode(Response.self, from: data)
 
-    }
-}
-
-extension ApiClient {
-    func refreshToken() async throws {
-        guard let refreshToken = KeychainService.shared.read(key: "refreshToken") else {
-            throw AuthServiceError.api(message: "Token invalid")
-        }
-        
-        let url = baseURL.appending(path: APIRoute.auth(.refreshToken).path)
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = HttpMethod.post.rawValue
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let payload = try JSONEncoder().encode(["refreshToken": refreshToken])
-        request.httpBody = payload
-        
-        let (data, response) = try await URLSession.shared.data(for: request)
-        
-        guard let httpResponse = response as? HTTPURLResponse else {
-            throw AuthServiceError.api(message: "Ocurred error")
-        }
-
-        guard (200...299).contains(httpResponse.statusCode) else {
-            KeychainService.shared.delete(key: "accessToken")
-            KeychainService.shared.delete(key: "refreshToken")
-            
-            return
-        }
-
-        let resultData = try JSONDecoder().decode(RefreshToken.self, from: data)
-        
-        KeychainService.shared.save(key: "accessToken", value: resultData.accessToken)
-        KeychainService.shared.save(key: "refreshToken", value: resultData.refreshToken)
-
-        
-        print("result", resultData)
-//        KeychainService.shared.save(key: "accessToken", value: resultData.)
-//        KeychainService.shared.save(key: "refreshToken", value: resultData.refreshToken())
-//        let refreshToken = keychainService.read(key: "refreshToken")
-//        let payload = try JSONEncoder().encode(["refreshToken": refreshToken])
-//        
-//        let requestModel = ApiRequest<RefreshToken>(method: .post, path: APIRoute.auth(.refreshToken).path, body: payload)
-//        
-//        do {
-//            let resultData = try await client.execute(requestModel)
-//            
-//            keychainService.save(key: "accessToken", value: resultData.accessToken)
-//            keychainService.save(key: "refreshToken", value: resultData.refreshToken)
-//
-//        } catch {
-//            print(error)
-//            throw error
-//        }
     }
 }
--- MiniFood/Networking/Interceptors/AuthInterceptors.swift
+++ MiniFood/Networking/Interceptors/AuthInterceptors.swift
@@ -6,3 +6,78 @@
 //
 
 import Foundation
+
+final class AuthInterceptors<Response: Decodable> {
+    var baseURL: URL
+    var decoder: JSONDecoder
+    var requestModel: ApiRequest<Response>
+    var headers: [String: String]
+    
+    init(baseURL: URL, decoder: JSONDecoder, requestModel: ApiRequest<Response>, headers: [String : String]) {
+        self.baseURL = baseURL
+        self.decoder = decoder
+        self.requestModel = requestModel
+        self.headers = headers
+    }
+    
+    func retry() async throws -> Response {
+        try await refreshToken()
+        
+        if let accessToken = KeychainService.shared.read(key: "accessToken"), !accessToken.isEmpty {
+            self.headers["Authorization"] = "Bearer \(accessToken)"
+        }
+        
+        let request = try requestModel.makeUrlRequest(defaultHeaders: headers)
+        
+        let (data, httpResponse) = try await URLSession.shared.data(for: request)
+        
+        guard let retryReponse = httpResponse as? HTTPURLResponse else {
+            throw URLError(.badServerResponse)
+        }
+        
+        guard (200...299).contains(retryReponse.statusCode) else {
+            let apiError = try? decoder.decode(ApiResponseError.self, from: data)
+            
+            throw AuthServiceError.api(
+                message: apiError?.error.message ?? "Request failed with status code \(retryReponse.statusCode)"
+            )
+            
+        }
+        
+        return try JSONDecoder().decode(Response.self, from: data)
+    }
+    
+    private func refreshToken() async throws {
+        guard let refreshToken = KeychainService.shared.read(key: "refreshToken") else {
+            throw AuthServiceError.api(message: "Token invalid")
+        }
+        
+        let url = baseURL.appending(path: APIRoute.auth(.refreshToken).path)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = HttpMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let payload = try JSONEncoder().encode(["refreshToken": refreshToken])
+        request.httpBody = payload
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw AuthServiceError.api(message: "Ocurred error")
+        }
+
+        guard (200...299).contains(httpResponse.statusCode) else {
+            KeychainService.shared.delete(key: "accessToken")
+            KeychainService.shared.delete(key: "refreshToken")
+            
+            return
+        }
+
+        let resultData = try JSONDecoder().decode(RefreshToken.self, from: data)
+        
+        KeychainService.shared.save(key: "accessToken", value: resultData.accessToken)
+        KeychainService.shared.save(key: "refreshToken", value: resultData.refreshToken)
+
+    }
+}
